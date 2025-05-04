31d4e088df2ef839e3195e5c91b9feb1eb217dc0
Jeongdonghun <arrrr180@konkuk.ac.kr>
2025-05-04 20:04:20 +0900

    feat: initial setting

---
+++ src/main/java/com/kufamilylinkbackend/KuFamilyLinkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kufamilylinkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuFamilyLinkBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KuFamilyLinkBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kufamilylinkbackend/KuFamilyLinkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kufamilylinkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuFamilyLinkBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
