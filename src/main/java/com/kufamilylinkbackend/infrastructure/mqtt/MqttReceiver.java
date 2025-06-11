c5ad49e2200d6f234564995b9d0422d75fea0172
kangchanwook <kan0202@naver.com>
2025-06-11 21:11:19 +0900

    [Feat] MqttReceiver 기능 완성

--- src/main/java/com/kufamilylinkbackend/infrastructure/mqtt/MqttReceiver.java
+++ src/main/java/com/kufamilylinkbackend/infrastructure/mqtt/MqttReceiver.java
@@ -40,7 +40,7 @@ public class MqttReceiver implements MqttCallback {
         AlertLog.builder()
             .fitbitUserId(null)
             .type(AlertType.INFO)
-            .title("MQTT 메시지 수신")
+            .title("비상 메시지 수신")
             .content(String.format("비상상황 발생! 피보호자가 위험 신호를 알렸습니다."))
             .fitbitUserId("CLC3TK")
             .createdAt(LocalDateTime.now())
