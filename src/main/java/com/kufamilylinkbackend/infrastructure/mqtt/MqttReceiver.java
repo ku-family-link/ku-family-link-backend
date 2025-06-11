96d95d75cd3356762191cbccec59a20bfed81c92
kangchanwook <kan0202@naver.com>
2025-06-11 20:55:44 +0900

    [Feat] MqttReceiver 기능 완성

--- src/main/java/com/kufamilylinkbackend/infrastructure/mqtt/MqttReceiver.java
+++ src/main/java/com/kufamilylinkbackend/infrastructure/mqtt/MqttReceiver.java
@@ -1,6 +1,10 @@
 package com.kufamilylinkbackend.infrastructure.mqtt;
 
+import com.kufamilylinkbackend.notification.AlertLog;
+import com.kufamilylinkbackend.notification.AlertType;
+import com.kufamilylinkbackend.notification.repository.AlertLogRepository;
 import jakarta.annotation.PostConstruct;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
@@ -15,6 +19,7 @@ import org.springframework.context.annotation.Configuration;
 public class MqttReceiver implements MqttCallback {
 
   private final MqttClient mqttClient;
+  private final AlertLogRepository alertLogRepository;
 
   @PostConstruct
   public void init() {
@@ -23,7 +28,7 @@ public class MqttReceiver implements MqttCallback {
 
   @Override
   public void connectionLost(Throwable throwable) {
-    System.out.println("연결이 끊어졌을 때 처리 로직");
+    System.out.println("연결이 끊어짐");
 
   }
 
@@ -31,11 +36,20 @@ public class MqttReceiver implements MqttCallback {
   public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
     log.info("메세지 도착 : topic: {} / message : {}", s, mqttMessage);
 
+    alertLogRepository.save(
+        AlertLog.builder()
+            .fitbitUserId(null)
+            .type(AlertType.INFO)
+            .title("MQTT 메시지 수신")
+            .content(String.format("topic: %s, message: %s", s, mqttMessage))
+            .createdAt(LocalDateTime.now())
+            .build());
+
   }
 
   @Override
   public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
-    System.out.println("메시지 전송이 완료되었을 때 처리 로직");
+    System.out.println("메시지 전송이 완료됨");
 
   }
 }
